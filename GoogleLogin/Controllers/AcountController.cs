029cabcb3590c117361ccf01aae2fb36b3a7c966
saranya chinthu <saranyachinthu@outlook.com>
2023-03-06 11:20:15 +0530

    Add project files.

---
+++ GoogleLogin/Controllers/AcountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Google;
+
+namespace GoogleLogin.Controllers
+{
+    public class AcountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            string returnUrl = "/Account/UserInfo";
+            return Challenge(new AuthenticationProperties { RedirectUri = returnUrl }, GoogleDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet]
+        public IActionResult UserInfo()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            var email = claimsIdentity.FindFirst(ClaimTypes.Email)?.Value;
+            var firstName = claimsIdentity.FindFirst(ClaimTypes.GivenName)?.Value;
+            var lastName = claimsIdentity.FindFirst(ClaimTypes.Surname)?.Value;
+
+            ViewData["Email"] = email;
+            ViewData["FirstName"] = firstName;
+            ViewData["LastName"] = lastName;
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            return SignOut(new AuthenticationProperties { RedirectUri = "/" }, CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+       
+    }
+}
---
+++ GoogleLogin/Program1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+
+namespace GoogleLogin
+{
+    public class Program1
+    {
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                var builder = WebApplication.CreateBuilder(args);
+
+                // Add services to the container.
+                builder.Services.AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = GoogleDefaults
+                    .AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddGoogle(GoogleDefaults.AuthenticationScheme, options =>
+                {
+
+                    options.ClientId = "463175777512-767h98o1cdg2k7f1ml95jn9n1b6pq6fs.apps.googleusercontent.com";
+                    options.ClientSecret = "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";
+                    options.ClaimActions.MapJsonKey("urn:google:picture", "picture", "url");
+                });
+
+                builder.Services.AddControllersWithViews();
+
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (!app.Environment.IsDevelopment())
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseRouting();
+                app.UseAuthentication();
+
+                app.UseAuthorization();
+
+                app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                app.Run();
+            }
+        }
+    }
+}
